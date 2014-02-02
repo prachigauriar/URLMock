3ee48e50f37474b97cb9ad7a706d0ea7d3c40612
Prachi Gauriar <prachi@twotoasters.com>
2014-02-02 09:04:53 -0500

    Update copyrights.

--- URLMock/Utilities/UMKTestUtilities.m
+++ URLMock/Utilities/UMKTestUtilities.m
@@ -3,7 +3,7 @@
 //  URLMock
 //
 //  Created by Prachi Gauriar on 5/29/2013.
-//  Copyright (c) 2013 Prachi Gauriar.
+//  Copyright (c) 2013–2014 Prachi Gauriar.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
