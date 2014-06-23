0935cc1325a29ca006fae4330817ee12b4b24bf8
Prachi Gauriar <prachi@twotoasters.com>
2014-06-23 19:16:58 -0400

    Add UMKURLSessionVerifier files

---
+++ Tests/Helpers/UMKURLSessionVerifier.m
@@ -0,0 +1,13 @@
+//
+//  UMKURLSessionVerifier.m
+//  URLMock
+//
+//  Created by Prachi Gauriar on 6/23/2014.
+//  Copyright (c) 2014 Two Toasters, LLC. All rights reserved.
+//
+
+#import "UMKURLSessionVerifier.h"
+
+@implementation UMKURLSessionVerifier
+
+@end
